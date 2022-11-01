9b4f09297f3d526792ab63d745b459397b4b193b
Shapna <shapna088@gmail.com>
2022-11-02 02:10:53 +0600

    added id api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -31,7 +31,15 @@ async function run() {
         const services = await cursor.toArray();
         res.send(services)
       })
-      
+
+      //2. specifically kono ekta id te jawar jonno API banate hobe
+      app.get('/services/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)}; // specific id'r neyar jonno
+        const service = await serviceCollection.findOne(query);
+        res.send(service)
+      })
+
     } finally {
       
     }
