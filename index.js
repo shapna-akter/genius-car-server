3b98653e79f2953d37a57183a8f4743b95f36987
Shapna <shapna088@gmail.com>
2022-11-02 17:21:29 +0600

    delete api

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
         res.send(orders)
       })
 
+      //5. delete API
+      app.delete('/orders/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await orderCollection.deleteOne(query);
+        res.send(result)
+      })
+
     } finally {
       
     }
