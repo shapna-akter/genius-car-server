e98c0c07aac75154dc164535437cb2632dc1b82c
Shapna <shapna088@gmail.com>
2022-11-04 01:49:51 +0600

    added token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -21,81 +22,89 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try {
-      const serviceCollection = client.db("geniusCar").collection("services"); 
-      const orderCollection = client.db('geniusCar').collection('orders'); //orders database
+  try {
+    const serviceCollection = client.db("geniusCar").collection("services");
+    const orderCollection = client.db('geniusCar').collection('orders'); //orders database
 
-      // 1. data load korar jonno API banabo
-      app.get('/services', async(req, res) =>{
-        const query = {} // sobgula k pawar jonno
-        const cursor = serviceCollection.find(query);
-        const services = await cursor.toArray();
-        res.send(services)
-      })
+    //JWT Token
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'});
+      res.send({token})
+    })
 
-      //2. specifically kono ekta id te jawar jonno API banate hobe
-      app.get('/services/:id', async(req, res) =>{
-        const id = req.params.id;
-        const query = {_id: ObjectId(id)}; // specific id'r neyar jonno
-        const service = await serviceCollection.findOne(query);
-        res.send(service)
-      })
+    // 1. data load korar jonno API banabo
+    app.get('/services', async (req, res) => {
+      const query = {} // sobgula k pawar jonno
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services)
+    })
 
-      //3. orders API
-      app.post('/orders', async(req, res) => {
-        const order = req.body;
-        const result = await orderCollection.insertOne(order);
-        res.send(result)
-      })
+    //2. specifically kono ekta id te jawar jonno API banate hobe
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }; // specific id'r neyar jonno
+      const service = await serviceCollection.findOne(query);
+      res.send(service)
+    })
 
-      //4. sob gulo order pawar jonno
-      app.get('/orders', async(req, res)=>{
-        let query = {}
-        if(req.query.email){
-            query={
-                email : req.query.email
-            }
+    //3. orders API
+    app.post('/orders', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result)
+    })
+
+    //4. sob gulo order pawar jonno
+    app.get('/orders', async (req, res) => {
+      let query = {}
+      if (req.query.email) {
+        query = {
+          email: req.query.email
         }
-        const cursor = orderCollection.find(query);
-        const orders = await cursor.toArray();
-        res.send(orders)
-      })
+      }
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders)
+    })
 
-       // 6. update 
-       app.patch('/orders/:id', async(req, res) =>{
-        const id = req.params.id;
-        const status = req.body.status;
-        const query = {_id: ObjectId(id)};
-        const updatedDoc = {
-          $set:{
-            status: status
-          }
+    // 6. update 
+    app.patch('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: status
         }
-        const result = await orderCollection.updateOne(query, updatedDoc)
-        res.send(result)
-       })
+      }
+      const result = await orderCollection.updateOne(query, updatedDoc)
+      res.send(result)
+    })
 
-       
-      //5. delete API
-      app.delete('/orders/:id', async(req, res) =>{
-        const id = req.params.id;
-        const query = {_id: ObjectId(id)};
-        const result = await orderCollection.deleteOne(query);
-        res.send(result)
-      })
 
-    } finally {
-      
-    }
+    //5. delete API
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result)
+    })
+
+  } finally {
+
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 
 //primary step get korbo
-app.get('/', (req, res) =>{
-    res.send('genius car server is running')
+app.get('/', (req, res) => {
+  res.send('genius car server is running')
 })
 // primary step listen korbo
-app.listen(port, (req, res) =>{
-    console.log(`genius car server running on ${port}`);
+app.listen(port, (req, res) => {
+  console.log(`genius car server running on ${port}`);
 })
